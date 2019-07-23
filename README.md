b8600925c89770309f703fab762e5ee81e5ba4c1
lufeng111 <1836232210@qq.com>
2019-07-23 18:17:59 +0800

    响应式表单

--- README.md
+++ README.md
@@ -28,5 +28,32 @@ To get more help on the Angular CLI use `ng help` or go check out the [Angular C
 
 ## 响应式表单
 <!-- 
+创建响应式表单的步骤：
+1：创建一个数据模型： 是指保存表单数据的数据结构，简称模型，它由定义在angular的Forms模块中的三个类FormControl, FormGroup, FormArray组成，
+FormControl：
+1：它是构成表单的基本单位，通常情况下它代表一个input元素，但是也可以代表一个更复杂的UI组件，比如日历，下拉选择块，FormControl保存着与其关联的html元素当前的值以及元素的校验状态，还有元素是否被修改过等信息，
+
+FormGroup：既可以代表表单的一部分，也可以代表整个表单，是多个FormControl的集合，FormGroup将多个FormControl的值和状态聚集在一起，如果一个FormControl是无效的，那么整个FormGroup就是无效的，在管理表单中多个相关联的字段时，FormGroup是很方便的，比如一个日期范围有两个input字段一个起始日期一个截止日期，这两个input就可以被放到一个FormGroup里面，当这两个字段中的任何一个值无效时，都会显示一个错误信息，
+
+FormArray：和FormGroup 是类似的，但是他有一个额外的长度属性，一般来说FormGroup 用来代表整个表单或者表单字段的一个固定的子集，比如前面看到reactive-form.component 中的 form和to这两个字段的固定的一个子集，FormArray用来代表可以增长的字段集合，举个例子表单上有email的框，一个用户可能有多个email，这时候就可以用FormArray，让用户输入任意数量的email地址，
+
+2：使用一些指令将模板中的html元素连接到数据模型上
+ -->
+## 响应式表单的类和指令, 这些指令和类全部都来自于ReactiveFormsModule模块
+<!-- 
+三个类：FormGroup， FormArray， FormControl
+三个类对应的指令：
+FormGroup： formGroup, formGroupName
+FormArray:  formArrayName
+FormControl: formControl，formControlName，
+
+formGroup和formControl 是属性绑定时使用的指令，FormArray不能通过属性绑定的方式来绑定的，
+formGroupName，formGroupName，formControlName是可以使用属性的名字来连接数据模型和dom元素的
+响应式表单是不可引用的（不可以声明模板本地变量来引用这个指令的实例），
+
+formArrayName: 和formControlName 一样，必须用在formGroup范围内，
+
+FormControl: 不能用在FormGroup 的内部，只能用在外部，
+注意：formGroup，formControl 都需要属性绑定语法 [formGroup]="formModel"， [formControl]="username"
 
  -->
--- src/app/reactive-form/reactive-form.component.ts
+++ src/app/reactive-form/reactive-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-form',
@@ -7,9 +8,60 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReactiveFormComponent implements OnInit {
 
+  /*
+username类型是FormControl ，FormControl 的构造函数会接收一个参数aaa ，这个参数用来指定这个FormControl 的初始值，
+这样写的话username: FormControl 的FormControl和页面上的input连接在一起的时候，那个input的初始值就是aaa,
+比如template-form.component中的<input ngModel name="mobile" type="number">，ngModel在这个input上，就会被input创建对应的FormControl
+*/
+  // username: FormControl = new FormControl("aaa");
+
+  // formModel类型是FormGroup,FormGroup的构造函数需要一个对象，
+  formModel: FormGroup = new FormGroup({
+    username: new FormControl("aaa"),
+
+    // 2: 起始日期范围
+    dateRange: new FormGroup({
+
+    from: new FormControl(),
+    to: new FormControl(),
+    }),
+     /*
+     FormArray中的FormControl没有名字，只有顺序号，一般情况下会与ngFor指令一起来使用formArrayName指令，
+
+     */
+    emails: new FormArray([
+      new FormControl("a@a.com"),
+      new FormControl("b@b.com"),
+    ])
+
+  })
+
+  // FormArray与FormGroup不同，他是没有相关的T(to,form)的，只能通过序号来访问FormArray 中的元素
+  // emails: FormArray = new FormArray([
+  //   //
+  //   new FormControl("a@a.com"),
+  //   new FormControl("b@b.com"),
+  // ])
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  onSubmit(){
+    // 1: 现在打印的是空的，是因为还没有在formModel中加任何属性，
+    console.log(this.formModel.value);
+  }
+
+  addEmail(){
+    /*
+    1：首先拿到当前的FormArray，
+    2: this.formModel.get('emails')拿到的是FormArray 类型的对象，需要转成FormArray类型as FormArray
+    3：往emails里push一个FormControl 就可以了
+    */
+   let emails = this.formModel.get('emails') as FormArray;
+   emails.push(new FormControl());
+
+
+  }
 }
